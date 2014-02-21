de0ed476e21f1d21d27a5005c291ed4e64815c5e
brad <bradleydchambers@gmail.com>
2014-02-20 20:07:13 -0500

    added echnoest calls

--- home/views.py
+++ home/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader, Context
+from pyechonest import config, artist
 
 def index(request):
     return render(request, 'index.html')
 
 def search(request):
-    c = Context({'qstring': request.GET['q']})
+    config.ECHO_NEST_API_KEY="REMOVED"
+
+    s_artist = request.GET['q']
+    s_results = artist.search(name=s_artist)
+
+    c = Context({'s_results': s_results})
+
 
     return render(request, 'result.html', c)
