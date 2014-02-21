dc1b312d3a49eb2e017c69c1d9d486e03f50dee7
brad <bradleydchambers@gmail.com>
2014-02-20 19:01:50 -0500

    redid form, added page-to-page comm

--- home/views.py
+++ home/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
-from django.template import RequestContext, loader
-from django.http import HttpResponse, HttpResponseRedirect
-from forms import single_music_form
+from django.template import RequestContext, loader, Context
 
-def index (request):
-    if request.method == 'POST': # If the form has been submitted...
-        form = single_music_form(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-            # Process the data in form.cleaned_data
-            
-			
-			
-            return HttpResponseRedirect('/result/') # Redirect after POST
-    else:
-        form = single_music_form() # An unbound form
+def index(request):
+    return render(request, 'index.html')
 
-    return render(request, 'index.html', {
-        'form': form,
-    })
+def search(request):
+    c = Context({'qstring': request.GET['q']})
+
+    return render(request, 'result.html', c)
--- music_search/urls.py
+++ music_search/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'home.views.index'),    
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'home.views.index'),   
+    url(r'^search/$', 'home.views.search'),
+    #url(r'^admin/', include(admin.site.urls)),
 )
