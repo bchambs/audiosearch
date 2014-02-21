c5ef792395716a7726ee218687a1d6405a41fc1d
brad <bradleydchambers@gmail.com>
2014-02-20 20:41:34 -0500

    more echonest calls

--- home/views.py
+++ home/views.py
@@ -6,12 +6,20 @@ def index(request):
     return render(request, 'index.html')
 
 def search(request):
+	#removed for security, add your key
     config.ECHO_NEST_API_KEY="REMOVED"
 
     s_artist = request.GET['q']
     s_results = artist.search(name=s_artist)
+    first = s_results[0]
 
-    c = Context({'s_results': s_results})
-
+    #print details of first match
+    c = Context({
+        'name': first.name,
+        'active': first.years_active, 
+        'hot': first.hotttnesss,
+        'songs': first.get_songs(results=25),
+        'similar': first.similar,
+    })
 
     return render(request, 'result.html', c)
