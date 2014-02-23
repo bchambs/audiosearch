68f130b1ec3fe9be42010979c7e7b0e08d83daad
brad <bradleydchambers@gmail.com>
2014-02-23 03:59:45 -0500

    added sorting

--- home/views.py
+++ home/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader, Context
-from pyechonest import config, artist
+from pyechonest import config, artist, song
+from operator import itemgetter
 
 def index(request):
     return render(request, 'index.html')
 
 def search(request):
-	#removed for security, add your key
-    config.ECHO_NEST_API_KEY="REMOVED"
+    #removed for security, add your key
+    config.ECHO_NEST_API_KEY="ULIQ4Q3WGU8MM4W2F"
 
-    s_artist = request.GET['q']
-    s_results = artist.search(name=s_artist)
-    first = s_results[0]
+    qs = request.GET['q']
+    
+    artists = artist.search(name=qs, sort='hotttnesss-desc', results=10)
+    songs = song.search(title=qs, sort='song_hotttnesss-desc', results=10)
 
-    #print details of first match
     c = Context({
-        'name': first.name,
-        'active': first.years_active, 
-        'hot': first.hotttnesss,
-        'songs': first.get_songs(results=25),
-        'similar': first.similar,
+    	'artists': artists,
+    	'songs': songs,
     })
 
     return render(request, 'result.html', c)
